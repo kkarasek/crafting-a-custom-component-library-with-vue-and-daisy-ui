29d5b5df7c8fd233141d8bccf68a4ed5c885cff0
Kuba Karasek <jakkarasek@gmail.com>
2024-09-23 11:30:00 +0200

    test hygen setup

---
+++ packages/library/src/components/DaisyBrowserMockup/DaisyBrowserMockup.story.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+import DaisyBrowserMockup from './DaisyBrowserMockup.vue';
+
+const state = reactive({});
+</script>
+<template>
+  <Story :layout="{ type: 'grid', width: '100%' }">
+    <Variant title="Variant Title">
+      <DaisyBrowserMockup url="https://localhost:3000"> Hi there 👋</DaisyBrowserMockup>
+    </Variant>
+
+    <template #controls></template>
+  </Story>
+</template>
